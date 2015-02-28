d09ce5bbd1cba2d220e51569650ab28bae19bec0
Hilmar K. Hallbjornsson <drupalviking@gmail.com>
2015-02-28 17:11:46 +0000

    Added page migrate file

---
+++ assets/db/page-migrate.sql
@@ -0,0 +1,53 @@
+update Page set body = "#Stjórnunarverðlaun Stjórnvísi
+Stjórnunarverðlaun Stjórnvísi eru veitt árlega þeim stjórnendum sem þykja hafa skarað framúr á sínu sviði. Félagar í Stjórnvísi tilnefna stjórnendur (1 eða fleiri), út frá þeim viðmiðum sem sett eru hverju sinni. Síðan tekur dómnefnd við öllum tilnefningargögnum og vinnur úr þeim. Dómnefnd birtir lista yfir þá sem hljóta lámarksfjölda tilnefninga. Stjórnunarverðlaunin voru veitt í fyrsta sinn í mars 2010.
+
+Forseti Íslands hr. Ólafur Ragnar Grímsson hefur afhent verðlaunin við hátíðlega athöfn í Veisluturninum Kópavogi.
+
+##Markmið
+Markmið Stjórnunarverðlauna Stjórnvísi er að vekja athygli á framúrskarandi starfi stjórnenda, sem eru aðilar að Stjórnvísi og hvetja félagsmenn til áframhaldandi faglegra starfa og árangurs.
+
+Það er Stjórnvísi mikið í mun að verðlaunin séu byggð á faglegu mati og hefur því verið vandað verulega til dómnefndar.
+
+##Dómnefnd 2015
+Dómnefnd 2015 skipa eftirtaldir:
+
++ Agnes Gunnarsdóttir, stundakennari við Háskólann á Bifröst
++ Ásta Bjarnadóttir, ráðgjafi Capacent
++ Bára Sigurðardóttir, formaður dómnefndar og mannauðsstjóri hjá Termu.
++ Borghildur Erlingsdóttir, forstjóri Einkaleyfastofu og formaður Félags forstöðumanna ríkisstofnana.
++ Gylfi Dalmann Aðalsteinsson, dósent við viðskiptafræðideild Háskóla Íslands.
++ Helgi Þór Ingason, dósent og forstöðumaður MPM náms við HR.
++ Hjörleifur Pálsson, stjórnarmaður og ráðgjafi.
+
+Ritari dómnefndar er Gunnhildur Arnardóttir framkvæmdastjóri Stjórnvísi
+
+##Viðmið dómnefndar
+Viðmið (e. criteries), sem dómnefnd leggur til grundvallar tilnefningum til Stjórnunarverðlaunanna eru eftirfarandi:
+
+###I. Árangursstjórnun
+Í starfi sínu stuðlar stjórnandinn að:
+
++ Framgangi stefnu fyrirtækisins / stofnunarinnar
++ Að lykilárangursþáttum sé náð
++ Öflugu kjarnastarfi
++ Markvissri upplýsingagjöf
+
+###II. Nýsköpun og þróun
+Í starfi sínu stuðlar stjórnandinn að:
+
++ Frumleika
++ Nýjum hugmyndum
++ Nýjum aðferðum og bættu vinnulagi
+
+###III. Forysta
+Stjórnandinn er:
+
++ Er óskoraður leiðtogi
++ Stuðlar að frelsi til athafna
++ Nýtur virðingar út fyrir eigin fyrirtæki / stofnun
+
+###IV. Rekstrarumhverfi
+Stjórnandinn:
+
++ Stuðlar að samkeppnisforskoti með skarpri framtíðarsýn og þekkingu á þörfum viðskiptavina
++ Hefur góðan skilning á atvinnugreininni ásamt samkeppnisaðilum og tengslum þessara aðila innbyrðis og við samfélagið" where id = 1;
