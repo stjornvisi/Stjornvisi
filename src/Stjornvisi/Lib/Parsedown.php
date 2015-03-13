0c0bfc26d1ec2ad0189158c7a58e221f28ffd9e5
einarvalur <fizk78@gmail.com>
2015-03-13 17:35:37 +1100

    disable number-dot as an ordered list in Markdown parser

--- src/Stjornvisi/Lib/Parsedown.php
+++ src/Stjornvisi/Lib/Parsedown.php
@@ -30,4 +30,89 @@ class Parsedown extends Markdown{
 
 	protected $breaksEnabled = true;
 
+
+	protected function blockList($Line)
+	{
+		list($name, $pattern) = $Line['text'][0] <= '-' ? array('ul', '[*+-]') : array('ul', '[*+-]');
+
+		if (preg_match('/^('.$pattern.'[ ]+)(.*)/', $Line['text'], $matches))
+		{
+			$Block = array(
+				'indent' => $Line['indent'],
+				'pattern' => $pattern,
+				'element' => array(
+					'name' => $name,
+					'handler' => 'elements',
+				),
+			);
+
+			$Block['li'] = array(
+				'name' => 'li',
+				'handler' => 'li',
+				'text' => array(
+					$matches[2],
+				),
+			);
+
+			$Block['element']['text'] []= & $Block['li'];
+
+			return $Block;
+		}
+	}
+
+	protected function blockListContinue($Line, array $Block)
+	{
+		if ($Block['indent'] === $Line['indent'] and preg_match('/^'.$Block['pattern'].'(?:[ ]+(.*)|$)/', $Line['text'], $matches))
+		{
+			if (isset($Block['interrupted']))
+			{
+				$Block['li']['text'] []= '';
+
+				unset($Block['interrupted']);
+			}
+
+			unset($Block['li']);
+
+			$text = isset($matches[1]) ? $matches[1] : '';
+
+			$Block['li'] = array(
+				'name' => 'li',
+				'handler' => 'li',
+				'text' => array(
+					$text,
+				),
+			);
+
+			$Block['element']['text'] []= & $Block['li'];
+
+			return $Block;
+		}
+
+		if ($Line['text'][0] === '[' and $this->blockReference($Line))
+		{
+			return $Block;
+		}
+
+		if ( ! isset($Block['interrupted']))
+		{
+			$text = preg_replace('/^[ ]{0,4}/', '', $Line['body']);
+
+			$Block['li']['text'] []= $text;
+
+			return $Block;
+		}
+
+		if ($Line['indent'] > 0)
+		{
+			$Block['li']['text'] []= '';
+
+			$text = preg_replace('/^[ ]{0,4}/', '', $Line['body']);
+
+			$Block['li']['text'] []= $text;
+
+			unset($Block['interrupted']);
+
+			return $Block;
+		}
+	}
 } 
