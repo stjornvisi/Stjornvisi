4e53d322e56845e802613737da0210864ed32507
einarvalur <fizk78@gmail.com>
2015-03-19 16:35:29 +1100

    error in PDO to Email service

--- Module.php
+++ Module.php
@@ -356,16 +356,16 @@ class Module{
 						$config = $sm->get('config');
 						$obj = new Email(
 							new \Stjornvisi\Lib\PDO(
+
+								$config['tracker']['dns'],
+								$config['tracker']['user'],
+								$config['tracker']['password'],
 								array(
-									'dns' => $config['tracker']['dns'],
-									'user' => $config['tracker']['user'],
-									'password' => $config['tracker']['password'],
-									'options' => array(
-										PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
-										PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-										PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-									)
+									PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+									PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+									PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
 								)
+
 							)
 						);
 						$obj->setEventManager( $sm->get('ServiceEventManager') );
